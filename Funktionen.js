c0b7003099c5b908db2371c392b8bdaf0c071e08
DeAloesler07 <lars.nussbaumer@edu.ksmenzingen.ch>
2023-05-05 22:11:44 +0200

    hello

--- Funktionen.js
+++ Funktionen.js
@@ -19,3 +19,14 @@ function topFunction() {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   document.documentElement.scrollLeft = 0;
 }
+
+function myFunction() {
+  // Get the snackbar DIV
+  var x = document.getElementById("snackbar");
+
+  // Add the "show" class to DIV
+  x.className = "show";
+
+  // After 3 seconds, remove the show class from DIV
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
